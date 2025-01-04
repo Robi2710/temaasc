7f830c3626464f552cc9cbecc99894137a1088aa
Robi2710 <102312717+Robi2710@users.noreply.github.com>
2025-01-04 14:51:16 +0200

    final bidimensional.cpp

--- bidimensional.cpp
+++ bidimensional.cpp
@@ -3,34 +3,37 @@
 using namespace std;
 ifstream fin("bid.in");
 ofstream fout("bid.out");
-int a[1024][1024]={0},desc,dim,nrfis,cnt,canfit=true;
-void ADD() {
+int a[1024][1024] = {0}, desc, dim, nrfis, cnt, canfit = true;
+void ADD()
+{
     fin >> nrfis;
-    
-    for (int k = 0; k < nrfis; k++) {
+
+    for (int k = 0; k < nrfis; k++)
+    {
         fin >> desc >> dim;
-        
-        // Calculate needed blocks (round up division)
-        cnt = (dim + 7) / 8; // Convert kB to blocks
-        
-        
+
+        cnt = (dim + 7) / 8; 
+
         bool found = false;
         int start_row = 0, start_col = 0;
-        
-        // Search for first available space
-        for (int i = 0; i < 1024 && !found; i++) {
-            for (int j = 0; j <= 1024 - cnt; j++) {
+
+        for (int i = 0; i < 1024 && !found; i++)
+        {
+            for (int j = 0; j <= 1024 - cnt; j++)
+            {
                 bool can_fit = true;
-                
-                // Check if we have enough consecutive free blocks
-                for (int l = 0; l < cnt; l++) {
-                    if (a[i][j + l] != 0) {
+
+                for (int l = 0; l < cnt; l++)
+                {
+                    if (a[i][j + l] != 0)
+                    {
                         can_fit = false;
                         break;
                     }
                 }
-                
-                if (can_fit) {
+
+                if (can_fit)
+                {
                     start_row = i;
                     start_col = j;
                     found = true;
@@ -38,32 +41,173 @@ void ADD() {
                 }
             }
         }
-        
-        if (found) {
-            // Fill the blocks with descriptor
-            for (int l = 0; l < cnt; l++) {
+
+        if (found)
+        {
+            for (int l = 0; l < cnt; l++)
+            {
                 a[start_row][start_col + l] = desc;
             }
-            
-            // Output result
-            fout << desc << ": ((" << start_row << ", " << start_col 
+
+            fout << desc << ": ((" << start_row << ", " << start_col
                  << "), (" << start_row << ", " << start_col + cnt - 1 << "))\n";
-        } else {
-            // No space found
+        }
+        else
+        {
             fout << desc << ": ((0, 0), (0, 0))\n";
+        }
+    }
+}
+void GET()
+{
+    fin >> desc;
+    int start = -1, end_column = -1, row, start_column = -1;
+    for (int i = 0; i < 1024; i++)
+    {
+        for (int j = 0; j < 1024; j++)
+        {
+            if (a[i][j] == desc)
+            {
+                if (start_column == -1)
+                {
+                    start_column = j;
+                }
+                end_column = j;
+                row = i;
+            }
+        }
+    }
+    if (start_column != -1)
+    {
+        fout << "((" << row << ", " << start_column << "), (" << row << ", " << end_column << "))\n";
+    }
+    else
+    {
+        fout << "((0, 0), (0, 0))\n";
+    }
+}
+void DELETE()
+{
+    fin >> desc;
+    for (int i = 0; i < 1024; i++)
+    {
+        for (int j = 0; j < 1024; j++)
+        {
+            if (a[i][j] == desc)
+            {
+                a[i][j] = 0;
+            }
+        }
+    }
+    for (int i = 0; i < 1024; i++)
+    {
+        for (int j = 0; j < 1024;)
+        {
+            if (a[i][j] != 0)
+            {
+                int row = i;
+                int start_column = j;
+                int currentDesc = a[i][j];
+                while (j < 1024 && a[i][j] == currentDesc)
+                {
+                    j++;
+                }
+                fout << currentDesc << ": ((" << row << ", " << start_column << "), (" << row << ", " << j - 1 << "))\n";
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
+}
+void DEFRAGMENTATION()
+{
+    int temp[1024][1024] = {0},index;
+    int currentRow = 0;
+    int currentCol = 0;
+    for (int i=0; i<1024; i++)
+    {
+        index=0;
+        for (int j=0; j<1024;)
+        {
+            if (a[i][j]!=0)
+            {
+                int currentDesc=a[i][j];
+                int cnt=0;
+                while (j+cnt<1024 && a[i][j+cnt]==currentDesc)
+                {
+                    cnt++;
+                }
+                if (currentCol+cnt>1024)
+                {
+                    currentRow++;
+                    currentCol=0;
+                }
+                for (int k=0; k<cnt; k++)
+                {
+                    temp[currentRow][currentCol+k]=currentDesc;
+                }
+                currentCol+=cnt;
+                j+=cnt;
+            }
+            else
+            {
+                j++;
+            }
+        }
+    }
+    for (int i=0; i<1024; i++)
+    {
+        for (int j=0; j<1024; j++)
+        {
+            a[i][j]=temp[i][j];
+        }
+    }
+    for (int i = 0; i < 1024; i++)
+    {
+        for (int j = 0; j < 1024;)
+        {
+            if (a[i][j] != 0)
+            {
+                int row = i;
+                int start_column = j;
+                int currentDesc = a[i][j];
+                while (j < 1024 && a[i][j] == currentDesc)
+                {
+                    j++;
+                }
+                fout << currentDesc << ": ((" << row << ", " << start_column << "), (" << row << ", " << j - 1 << "))\n";
+            }
+            else
+            {
+                j++;
+            }
         }
     }
 }
 int main()
 {
-    int nrinstr,instr;
-    fin>>nrinstr;
-    for (int i=1; i<=nrinstr; i++)
+    int nrinstr, instr;
+    fin >> nrinstr;
+    for (int i = 1; i <= nrinstr; i++)
     {
-        fin>>instr;
-        if (instr==1)
+        fin >> instr;
+        if (instr == 1)
         {
             ADD();
+        }
+        else if (instr == 2)
+        {
+            GET();
+        }
+        else if (instr == 3)
+        {
+            DELETE();
+        }
+        else if (instr == 4)
+        {
+            DEFRAGMENTATION();
         }
     }
 }
